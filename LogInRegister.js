748f187bf9f4c716f6fb906dd09e512e2c13db38
GitterJakub <ja.baranec@gmail.com>
2022-07-07 15:35:36 +0200

    Login Konzept hinzugefügt

--- LogInRegister.js
+++ LogInRegister.js
@@ -1,14 +1,53 @@
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
+import {myAdmin} from "./api/api";
+//import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+//import {...} from "firebase/auth";
+//import {...} from "firebase/database";
+//import {...} from "firebase/firestore";
+//import {...} from "firebase/functions";
+//import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: 'api-key',
+    authDomain: 'project-id.firebaseapp.com',
+    databaseURL: 'https://project-id.firebaseio.com',
+    projectId: 'project-id',
+    storageBucket: 'project-id.appspot.com',
+    messagingSenderId: 'sender-id',
+    appId: 'app-id',
+    measurementId: 'G-measurement-id',
+};
+
+//initializeApp(firebaseConfig);
+
+
 export function LogInRegister() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = () => {
+        if (username === myAdmin.username && password === myAdmin.password){
+            console.log("success");
+
+        }else {
+            console.log("not");
+            console.log(username+ "  " + myAdmin.username);
+            console.log(password + "   " + myAdmin.password)
+        }
+    }
 
     return(
         <View>
             <Text>Login</Text>
             <Text>Username: </Text>
-            <TextInput></TextInput>
+            <TextInput value={username} onChangeText={setUsername}></TextInput>
             <Text>Password: </Text>
-            <TextInput></TextInput>
-            <Button title={"Log in"}  ></Button>
+            <TextInput value={password} onChangeText={setPassword}></TextInput>
+            <Button title={"Log in"} onPress={login} ></Button>
             <Text>OR</Text>
             <Button title={"Register"}></Button>
         </View>
---
+++ api/api.js
@@ -0,0 +1,19 @@
+
+
+const init = {
+  "type": "service_account",
+  "project_id": "my-awesome-project-d5e8f",
+  "private_key_id": "921006631f4cdc43dff34bbcf6283da390300f37",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDDcb4Qn2F9oBl/\nNsUuxI7bvh7cHqde4JWWvjp5hTFceENwsO8tOlNeXlL1YZRFh6xV9wo1Es1uH/13\nSBEDiPNgofvCGcL3zzqYAcu95VMVM65QcZUIxsGX79+PyHyZBZZzisHol7OVuxMR\nHuVp15O9V4jVduwN0Lw2CF1usdeyOVjMZQTB2taaCxuGau7AqhLIIo8QxFJMNYr3\n17MXdK2azPBpgU78+yxSPLLXn45OW1SyPj+VX1sT8vqphXiLZYhfA8BZOGTNVHos\nLHdUbZsgzd4w1GXjtNecAC0f0SCT3jIdUrcZGQPb664eV0rUGnBc9KprNKLK3kB3\nlR/8/tQ1AgMBAAECggEABhcTwfHYIwbWSeQGMJxjCo7kkvMI6nMbcNyRoZAYcdYK\nMpyY1LMgvkE4hY6aWluixPT95kR6jp1u6QTKXu6pYs/5XTAvUUiC/hHu7DEVWITz\n6sF3UD7dRnjs4aPw2gCAvS324o1jml1UzUTwsiPLu0Ewhtz9+GTnkH7MkTKifYUF\nAfpkFdEC2PtKpkGWPajTJEaqTPMiel4Z5nv43vWUmZ4kRb/uk2pFz+s6s0wpOanC\ntBHcshQGNXfLsP8eL1HWCc87aEy6TKyj51EgXbvp81Ty0NJskqKrK13JAZhVXZ1p\n55n9L3N2pdqWX1mFOxpG2XGSpz4+hcY3dlkvSNXImwKBgQDhtAk5dy5UDYpU6sI0\nGWlKKBPFDXt4oaU9T2mvsreO6xWldNpty9BJhm4TDmK7pJxNrnBM6Px4wmeRE9Yj\n0SDfFInXcCkjzdkb3KdU7Ls8nys4Df/OG0GjyImH8pYhCpXR3lZaF0qppbn5b0C9\n+nDy4JDKDXWxVd4fyzrvo52UowKBgQDdrea+lW/xIrSCzgXkAtwlm0wppVZrNJ0Z\nFLEI5ZylG2NmoaPPM5wuIfFwBY08ke4dnw0NhzYTIAyCnpaXjP2kt9D8ILjZfHhv\nAmabnzc5eW/nFDI/cMuP1W7eQAMoEylOTBP6YSRwfEfy/ok95tLX1ziYXyM8wKT2\noPKAfdmpRwKBgHbc66a530ogv2MtJw1s/IgzRwYVrZq/9lethMkk44p+v62dNpNw\nLMcr5rhecRzE+hPm8/ltEdk++G/s9G90aU1Sca1lwiS6+cW0lOMisRCh+o7QwJD4\nxb7Nk7Bmzy2jd3dJEkzry0RApQp5zqy+QdGG9nmSURMNV6eCzgz6K7jlAoGAX9w3\nj+qnQas0S+wzd+oUsN57DPdGa42Q8auU8rf4CR3S2irD33H986UR9tMvxCWGxqBk\n0JjLtXGCd2mEzXxWosgqQx0TUlkU60wqetla1CvDTDXblLt+C3DMRKeBte6zSfx0\nFy2yyEG2C0oupuoN+vBCtQFPB5J2w3/N3eqOGTkCgYEAhhn2+Rj0pHAp+oicprLt\nf7U6flMiMRMYk5jxCZTu8LIq/9ZEaUIUOCMK8rQ5r/eytUafNvkGF5JIkoEBmoeV\nd/bH2EVeoqCZKSHn8rULkMQkiC8J8d3Fg5i1d0AId052nlrjIaus4gVbtA9lawBl\nHhM53HUFwM3d2v0etUOfnXY=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-rdu3c@my-awesome-project-d5e8f.iam.gserviceaccount.com",
+  "client_id": "113035275635627280477",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rdu3c%40my-awesome-project-d5e8f.iam.gserviceaccount.com"
+}
+
+export const myAdmin = {
+  username: "admin",
+  password: "Admin1234Zli"
+}
