8cf8654805b8e7a2ba73bbc3ff460e541b091888
jabar <ja.baranec@gmail.com>
2022-07-08 10:36:38 +0200

    Api und Pedometer Api für die Excerisces Pedometer funktioniert nur auf Iphone

--- Screens/Fitnesscourse.js
+++ Screens/Fitnesscourse.js
@@ -1,9 +1,39 @@
-import {SafeAreaView, Text} from "react-native";
+import {Button, SafeAreaView, Text, TextInput} from "react-native";
+import responderTouchHistoryStore from "react-native-web/dist/modules/useResponderEvents/ResponderTouchHistoryStore";
+import {fetch} from "react-native/Libraries/Network/fetch";
+import {useState} from "react";
+
+let exc = ['hello'];
 
 export function Fitnesscourse(){
+
+    const [data, setData] = useState();
+    const [bodyPart, setBodyPart] = useState("");
+
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '99471330c5mshe6c4ddecc33bd6cp1c969ajsn58790eddaa5a',
+            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+        }
+    };
+
+
+
+    const place = () => {
+        fetch('https://exercisedb.p.rapidapi.com/exercises/bodypart/'+bodyPart.toLowerCase(), options)
+            .then(response => response.json())
+            .then(data => setData(JSON.stringify(data)))
+            .catch(err => console.error(err));
+    }
+
     return(
         <SafeAreaView>
             <Text>Your Courses</Text>
+            <TextInput placeholder={"A Body Part"}  value={bodyPart} onChangeText={setBodyPart}/>
+            <Button title={"show"} onPress={place}/>
+            <Text>{data}</Text>
         </SafeAreaView>
     )
 }
--- Screens/Stepscount.js
+++ Screens/Stepscount.js
@@ -1,9 +1,67 @@
-import {SafeAreaView, Text} from "react-native";
+import {SafeAreaView, Text, StyleSheet} from "react-native";
+import { Pedometer } from 'expo-sensors';
+
 
 export function Stepscount() {
+    let state = {
+        isPedometerAvailable: 'checking',
+        pastStepCount: 0,
+        currentStepCount: 0,
+    };
+
+    function componentDidMount() {
+        _subscribe();
+    }
+
+    function componentWillUnmount() {
+        this._unsubscribe();
+    }
+
+    let _subscribe = () => {
+        this._subscription = Pedometer.watchStepCount(result => {
+            setState({
+                currentStepCount: result.steps,
+            });
+        });
+
+        Pedometer.isAvailableAsync().then(
+            result => {
+                setState({
+                    isPedometerAvailable: String(result),
+                });
+            },
+            error => {
+                setState({
+                    isPedometerAvailable: 'Could not get isPedometerAvailable: ' + error,
+                });
+            }
+        );
+
+        const end = new Date();
+        const start = new Date();
+        start.setDate(end.getDate() - 1);
+        Pedometer.getStepCountAsync(start, end).then(
+            result => {
+                setState({ pastStepCount: result.steps });
+            },
+            error => {
+                setState({
+                    pastStepCount: 'Could not get stepCount: ' + error,
+                });
+            }
+        );
+    };
+
+    let _unsubscribe = () => {
+        this._subscription && this._subscription.remove();
+        this._subscription = null;
+    };
     return(
         <SafeAreaView>
             <Text>There are Steps</Text>
+            <Text>Pedometer.isAvailable: {state.isPedometerAvailable}</Text>
+            <Text>Steps taken in the last 24h: {state.pastStepCount}</Text>
+            <Text>Walk! : {state.currentStepCount}</Text>
         </SafeAreaView>
     )
 }
